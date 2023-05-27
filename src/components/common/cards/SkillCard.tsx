4182a795153cfebe28be42685abc96994ca1b519
shahariar50 <shahariar.touhid@gmail.com>
2023-05-27 19:39:34 +0600

    responsive skill section

--- src/components/common/cards/SkillCard.tsx
+++ src/components/common/cards/SkillCard.tsx
@@ -3,8 +3,8 @@ import { SkillCardType } from "./cardTypes";
 
 const SkillCard: React.FC<SkillCardType> = ({ name, experience, logo }) => {
   return (
-    <div className="flex w-full gap-4 p-3 bg-gray-200 rounded-md items-center">
-      <div className="w-12 h-12 shrink-0">
+    <div className="flex w-full gap-2 lg:gap-4 p-2 lg:p-3 bg-gray-200 dark:bg-gray-700 rounded-md items-center h-full">
+      <div className="w-10 lg:w-12 h-10 lg:h-12 shrink-0">
         <Image
           className="h-full w-full object-contain"
           src={logo || ""}
@@ -15,7 +15,9 @@ const SkillCard: React.FC<SkillCardType> = ({ name, experience, logo }) => {
       </div>
       <div className="grow">
         <h5>{name}</h5>
-        <p className="m-0 text-gray-500 text-sm">{experience}</p>
+        <p className="m-0 text-gray-500 dark:text-gray-400 text-xs lg:text-sm">
+          {experience}
+        </p>
       </div>
     </div>
   );
